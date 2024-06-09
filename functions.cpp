c2ac2cfc1bb93b7a61dee5b3b3241251ee55f3b3
Aryan Jadav <141330989+aryanjadav037@users.noreply.github.com>
2024-06-09 23:19:46 +0530

    C++ solution

--- functions.cpp
+++ functions.cpp
@@ -20,10 +20,7 @@ int max_of_four(int a,int b,int c,int d){
 
 int main(){
     int a, b, c, d;
-    cin>>a;
-    cin>>b;
-    cin>>c;
-    cin>>d;
+    cin>>a>>b>>c>>d;
     int ans = max_of_four(a, b, c, d);
     cout<<ans;
     
